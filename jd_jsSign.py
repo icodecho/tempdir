5ab4e3c802c6a691a581688178d23e0574e45cae
icodecho <9eeks.lover@gmail.com>
2021-10-22 19:02:52 +0800

    Update jd_jsSign.py

--- jd_jsSign.py
+++ jd_jsSign.py
@@ -31,7 +31,9 @@ class Util(object):
             if len(token_list) >= 1:
                 print("从配置文件中获取到{}个token".format(len((token_list))))
                 print(token_list)
-                token_list_0 = token_list[0][0:(len(token_list[0])-1)]
+                token_list_0 = token_list[0]
+                print(token_list_0)
+                token_list_0=token_list_0[0:(len(token_list[0])-1)]
                 print(token_list_0)
                 return token_list_0
             else:
