7389cbe172ce27dcd8e4f48286c665fc1e791059
icodecho <9eeks.lover@gmail.com>
2026-05-24 18:26:40 +0800

    Update mgtv-child.js

--- tv/drpy2/mgtv-child.js
+++ tv/drpy2/mgtv-child.js
@@ -37,7 +37,7 @@ let the_match = the_origin_url.match(the_regex);
 let the_vid;
 let the_cid;
 let base_vod;
-let the_reqHeaders = new Headers();
+let the_reqHeaders = {};
 the_reqHeaders.append("accept", "application/json, text/plain, */*");
 the_reqHeaders.append("origin", "https://www.mgtv.com");
 the_reqHeaders.append("referer", "https://www.mgtv.com/");
