fd49a47bb97ccc35354bfea918a6c8a649163602
icodecho <9eeks.lover@gmail.com>
2026-05-24 18:24:58 +0800

    Update mgtv-child.js

--- tv/drpy2/mgtv-child.js
+++ tv/drpy2/mgtv-child.js
@@ -32,20 +32,29 @@ var rule = {
 //https://www.mgtv.com/b/611790/21155332.html?fpa=1566&fpos=&lastp=ch_child
 let the_origin_url = "https://www.mgtv.com/b/611790/21155332.html?fpa=1566&fpos=&lastp=ch_child";
 let the_regex = /\/b\/(\d+)\/(\d+)\.html/;
-print("调试日志");
 print(input);
 let the_match = the_origin_url.match(the_regex);
 let the_vid;
 let the_cid;
 let base_vod;
+let the_reqHeaders = new Headers();
+the_reqHeaders.append("accept", "application/json, text/plain, */*");
+the_reqHeaders.append("origin", "https://www.mgtv.com");
+the_reqHeaders.append("referer", "https://www.mgtv.com/");
+the_reqHeaders.append("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36");
+let the_reqOptions01 = {
+    method: "GET",
+    headers: the_reqHeaders,
+    redirect: "follow"
+};
 if (the_match) {
     the_cid = the_match[1]; // "611790"
     the_vid = the_match[2]; // "21155332"
     let the_info_url = "https://pcweb.api.mgtv.com/video/info?allowedRC=1&vid=" + the_vid + "&cid=" + the_cid + "&type=b&_support=10000000";
-    let the_info_data = JSON.parse(fetch(the_info_url, {method: "GET",redirect: "follow"}));
-    print("调试日志");
-	print(the_info_data);
-	if (the_info_data.code == 200) {
+	let the_req_ret_raw = fetch(the_info_url, the_reqOptions01);
+	print(the_req_ret_raw);
+    let the_info_data = JSON.parse(the_req_ret_raw);
+    if (the_info_data.code == 200) {
         let the_vdata = the_info_data.data;
         let the_vdata_info = the_vdata.info;
         base_vod = {
@@ -63,7 +72,10 @@ if (the_match) {
     let the_size = 30;
     //https://pcweb.api.mgtv.com/episode/list?_support=10000000&version=5.5.35&video_id=21155332&page=0&size=30&platform=4&src=mgtv
     let the_playlist_url = "https://pcweb.api.mgtv.com/episode/list?_support=10000000&version=5.5.35&video_id=" + the_vid + "&page=" + the_page + "&size=" + the_size + "&platform=4&src=mgtv";
-    let the_playlist_data = JSON.parse(fetch(the_playlist_url, {method: "GET",redirect: "follow"}));
+    let the_playlist_data = JSON.parse(fetch(the_playlist_url, {
+                method: "GET",
+                redirect: "follow"
+            }));
     if (the_playlist_data.code == 200) {
         let the_pdata = the_playlist_data.data;
         let the_ptotal = the_pdata.total;
@@ -83,7 +95,7 @@ if (the_match) {
             playList = vodItems.join("#")
         }
         if (playList.length > 0) {
-			
+
             vod_play["mgtv"] = playList;
             //
             let tabs = Object.keys(vod_play);
